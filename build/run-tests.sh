e006f9152e1cd9eae4c39a3be1b2f145b3894c87
mtestrot <13015102+mtestrot@users.noreply.github.com>
2019-04-24 09:59:01 +0200

    Add external test script

---
+++ build/run-tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# cf. https://stackoverflow.com/questions/1489277/how-to-use-prune-option-of-find-in-sh for find with prune
+# cf. https://stackoverflow.com/a/4667725 for process substitution < < (find...)
+
+while read f
+do
+    cd ${f}; go test ./... ; (( exit_status = exit_status || $? ))
+done < <(find $PWD -maxdepth 1 \( -name .git -o -name .idea -o -name build \) -prune -o ! -path $PWD -type d -print )
+
+exit ${exit_status}
